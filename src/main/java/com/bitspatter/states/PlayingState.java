e946fe4bd472bf0b7777b2e994aaa5cbb4c52917
Jørgen P. Tjernø <jorgenpt@gmail.com>
2013-01-03 22:24:20 -0800

    Switch to StateBasedGame.

---
+++ src/main/java/com/bitspatter/states/PlayingState.java
@@ -0,0 +1,205 @@
+package com.bitspatter.states;
+
+import org.newdawn.slick.*;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.state.*;
+
+import com.bitspatter.Board;
+import com.bitspatter.Piece;
+import com.bitspatter.renderer.BlockRenderer;
+
+public class PlayingState extends BasicGameState implements MouseListener {
+    public final static int STATE_ID = 1;
+    final int SECONDS_PER_STEP = 1;
+    final int BOARD_MARGIN = 10;
+    final int BOARD_WIDTH = 10, BOARD_HEIGHT = 22;
+
+    // Board including landed pieces.
+    Board board;
+    // Currently dropping piece.
+    Piece currentPiece;
+
+    BlockRenderer blockRenderer;
+    Rectangle boardRenderArea;
+
+    // Number of ms until we do a "soft drop" (i.e. move the current piece one step down)
+    int msTillNextStep;
+
+    // Whether or not we're currently in "warp" mode, i.e. game paused and allowing player to move blocks.
+    boolean warping = false;
+
+    // This is the delta between the mouse pointer and the top left of the currently dragged block, to make dragging
+    // feel natural.
+    int dragOffsetX, dragOffsetY;
+
+    @Override
+    public int getID() {
+        return STATE_ID;
+    }
+
+    @Override
+    public void render(GameContainer gc, StateBasedGame game, Graphics g) throws SlickException {
+        if (warping) {
+            board.renderWarping(g, boardRenderArea);
+        }
+
+        currentPiece.render(g);
+
+        if (warping) {
+            Input input = gc.getInput();
+            g.setClip(boardRenderArea);
+            currentPiece.renderDraggable(g, input.getMouseX() - dragOffsetX, input.getMouseY() - dragOffsetY);
+            g.clearClip();
+        }
+
+        board.render(g, boardRenderArea, warping);
+    }
+
+    @Override
+    public void init(GameContainer gc, StateBasedGame game) throws SlickException {
+        initializeRenderer(gc);
+
+        board = new Board(BOARD_WIDTH, BOARD_HEIGHT, blockRenderer);
+
+        Piece.createPieces(blockRenderer);
+        currentPiece = Piece.getRandomPiece();
+
+        initializeInput(gc);
+
+        msTillNextStep = SECONDS_PER_STEP * 1000;
+    }
+
+    private void initializeInput(GameContainer gc) {
+        Input input = gc.getInput();
+        input.enableKeyRepeat();
+        input.addMouseListener(this);
+    }
+
+    private void initializeRenderer(GameContainer gc) {
+        int boardHeight = gc.getHeight() - BOARD_MARGIN * 2;
+        int blockSize = boardHeight / BOARD_HEIGHT;
+        boardHeight = blockSize * BOARD_HEIGHT;
+        float actualYMargin = (gc.getHeight() - boardHeight) / 2.0f;
+
+        boardRenderArea = new Rectangle(BOARD_MARGIN, actualYMargin, blockSize * BOARD_WIDTH, boardHeight);
+        blockRenderer = new BlockRenderer(boardRenderArea, blockSize);
+    }
+
+    private void toggleWarping() {
+        if (!warping && currentPiece.warped)
+            return;
+
+        warping = !warping;
+        if (!warping) {
+            currentPiece.stopDrag();
+        }
+    }
+
+    @Override
+    public void update(GameContainer gc, StateBasedGame game, int delta) throws SlickException {
+        Input input = gc.getInput();
+        if (input.isKeyPressed(Input.KEY_SPACE)) {
+            toggleWarping();
+        }
+
+        if (warping) {
+            return;
+        }
+
+        board.update(delta);
+
+        if (input.isKeyPressed(Input.KEY_LEFT)) {
+            movePieceHorizontally(-1);
+        } else if (input.isKeyPressed(Input.KEY_RIGHT)) {
+            movePieceHorizontally(1);
+        }
+
+        if (input.isKeyPressed(Input.KEY_Z)) {
+            rotatePiece(false);
+        } else if (input.isKeyPressed(Input.KEY_X)) {
+            rotatePiece(true);
+        }
+
+        if (input.isKeyPressed(Input.KEY_DOWN)) {
+            msTillNextStep = SECONDS_PER_STEP * 1000;
+            lowerPiece();
+        } else if (input.isKeyPressed(Input.KEY_UP)) {
+            while (!lowerPiece())
+                ;
+        } else {
+            msTillNextStep -= delta;
+            if (msTillNextStep < 0) {
+                msTillNextStep += SECONDS_PER_STEP * 1000;
+                lowerPiece();
+            }
+        }
+    }
+
+    private void rotatePiece(boolean clockwise) throws SlickException {
+        Piece newPiece = currentPiece.rotated(clockwise);
+        if (board.nudgeToValid(newPiece)) {
+            currentPiece = newPiece;
+        }
+    }
+
+    private void movePieceHorizontally(int steps) {
+        currentPiece.topLeftX += steps;
+        if (board.pieceLanded(currentPiece)) {
+            currentPiece.topLeftX -= steps;
+        }
+    }
+
+    private boolean lowerPiece() throws SlickException {
+        currentPiece.topLeftY++;
+        if (board.pieceLanded(currentPiece)) {
+            currentPiece.topLeftY--;
+            if (board.finalizePiece(currentPiece)) {
+                System.exit(0);
+            }
+
+            currentPiece = Piece.getRandomPiece();
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean isAcceptingInput() {
+        return warping;
+    }
+
+    @Override
+    public void mousePressed(int button, int x, int y) {
+        if (button != 0) {
+            return;
+        }
+
+        int blockX = blockRenderer.getBlockX(x);
+        int blockY = blockRenderer.getBlockX(y);
+        if (!currentPiece.contains(blockX, blockY)) {
+            return;
+        }
+
+        dragOffsetX = x - blockRenderer.getX(blockX);
+        dragOffsetY = y - blockRenderer.getY(blockY);
+        currentPiece.startDrag(blockX, blockY);
+    }
+
+    @Override
+    public void mouseReleased(int button, int x, int y) {
+        if (button == 0) {
+            int blockX = blockRenderer.getBlockX(x);
+            int blockY = blockRenderer.getBlockX(y);
+            if (board.contains(blockX, blockY)) {
+                if (currentPiece.stopDrag(blockX, blockY)) {
+                    currentPiece.warped = true;
+                    warping = false;
+                    board.mutate();
+                }
+            } else {
+                currentPiece.stopDrag();
+            }
+        }
+    }
+}
